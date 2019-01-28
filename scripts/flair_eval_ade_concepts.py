e9131d63055ec1855f19fbb7c9ac785199493f73
Tim Miller <timothy.miller@childrens.harvard.edu>
2019-01-27 21:01:23 -0500

    Change to use flair tokenization at eval time for concepts.

--- scripts/flair_eval_ade_concepts.py
+++ scripts/flair_eval_ade_concepts.py
@@ -41,9 +41,10 @@ def main(args):
     txt_files = glob.glob(join(args.data_dir, '*.txt'))
 
     sent_splitter = PunktSentenceTokenizer()
-    tokenizer = TreebankWordTokenizer()
+    #tokenizer = TreebankWordTokenizer()
 
     for txt_fn in txt_files:
+        print("Processing %s" % (basename(txt_fn)))
         ann_fn = join(args.output_dir, basename(txt_fn)[:-3]+'ann')
         with open(txt_fn, 'r') as myfile:
             text = myfile.read()
@@ -57,9 +58,10 @@ def main(args):
         for sent_span in sent_spans:
             raw_offset = sent_span[0]
             sent = text[sent_span[0]:sent_span[1]]
-            tokens = tokenizer.tokenize(sent)
+            #tokens = tokenizer.tokenize(sent)
             # tagged = pos_tag(tokens)
-            flair_sent = Sentence(' '.join(tokens))
+            #flair_sent = Sentence(' '.join(tokens))
+            flair_sent = Sentence(sent, use_tokenizer=True)
             ade_tagged = tagger.predict(flair_sent)
            
             cmap = raw_flair_charmap(sent, flair_sent.to_tokenized_string()) 
@@ -78,8 +80,8 @@ def main(args):
                     start = entity['start_pos']
                     end = entity['end_pos']
                     
-                    raw_start = cmap[start] 
-                    raw_end = cmap[end]
+                    raw_start = start #cmap[start] 
+                    raw_end = end #cmap[end]
                     
     #                print('Mapped entity type %s(%s):(%d, %d) => (%d, %d)' % (entity['type'], entity['text'], start, end, raw_offset+raw_start, raw_offset+raw_end) )
                     ann_out.write('T%d\t%s %d %d\t%s\n' % (ent_id, entity['type'], raw_offset+raw_start, raw_offset+raw_end, entity['text']))
